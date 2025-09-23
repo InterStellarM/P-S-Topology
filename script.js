f0290efa94b0bd81ea72aee11411becd34550d05
InterStellarM <Jarvisprotoss@gmail.com>
2025-09-23 12:36:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ const gridData = {
         //                                              R        R_l      R_l^2    R_K      w-pro    w-uni    w-box     J-pro    J-box    I_0      I_0^2
         { label: "1st countable",               cells: ["green", "green", "green", "green", "green", "green", "red",    "gray", "gray",   "gray",  "green"] },
         { label: "2nd countable",               cells: ["green", "red",   "red",   "green", "green", "red",   "red",    "gray", "gray",   "gray",  "red"] },
-        { label: "Separable",                   cells: ["green", "gray", "gray", "green", "green", "red",   "red",    "gray", "gray",   "gray",  "red"] },
-        { label: "Lindelöf",                    cells: ["green", "green", "red",   "green", "green", "red",   "red",    "gray", "gray",   "gray",  "green"] },
+        { label: "Separable",                   cells: ["green", "gray", "gray", "green", "green", "red",   "gray",    "gray", "gray",   "gray",  "red"] },
+        { label: "Lindelöf",                    cells: ["green", "green", "red",   "green", "green", "red",   "gray",    "gray", "gray",   "gray",  "green"] },
         { label: "Hausdorff (T2)",              cells: ["green", "green", "green", "green", "green", "green", "green",  "gray", "green",  "gray",  "gray"] },
         { label: "Regular (T3)",                cells: ["green", "green", "green", "red",   "green", "green", "green",  "gray", "green",  "gray",  "gray"] },
         { label: "Completely Regular (T3.5)",   cells: ["green", "green", "green", "red",   "green", "green", "gray",  "gray", "green",  "gray",  "gray"] },
